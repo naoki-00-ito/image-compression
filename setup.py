1e7e7764f0dbf3400961e95ab6ab74fbf5f322bd
Naoki_Ito <so30.itonaoki@gmail.com>
2022-02-20 15:10:04 +0900

    ディレクトリ構成作成

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="imgcompr",
+	version='1.1',
+  description='画像圧縮ツール',
+  author='Ito Naoki',
+  author_email='so30.itonaoki@gmail.com',
+  url='https://github.com/ItoNaoki/image-compression',
+  packages=find_packages(),
+  entry_points={
+    'console_scripts':[
+      'imgcompr = imgcompr.cli:execute',
+    ],
+  },
+)
